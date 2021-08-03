42264f14022b96d6e9d9819d5c6fbac05885d694
AlbertoCanales1 <albertocanales1@outlook.com>
2021-08-02 19:04:35 -0500

    Updated so notes can read db.json file

--- server.js
+++ server.js
@@ -27,4 +27,7 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, "/public/assets/index.html"))
 });
 
+app.get("api/notes", (req, res) => {
+  res.sendFile(path.join(__dirname, "./db/db.json"))
+});
 
